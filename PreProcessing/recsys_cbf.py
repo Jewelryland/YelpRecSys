ed0665e0903f468ae4c797799dd0fde81e2db571
Eddie <adward_r@icloud.com>
2016-04-20 16:16:19 +0800

    'recsys_cbd' as Baseline Model with useful code pieces

--- PreProcessing/recsys_cbf.py
+++ PreProcessing/recsys_cbf.py
@@ -19,6 +19,7 @@ from sklearn.cross_validation import StratifiedKFold, ShuffleSplit
 from sklearn.metrics import *
 from sklearn.naive_bayes import GaussianNB
 from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
+from sklearn.decomposition import PCA
 
 # Constant values
 DATA_PATH = '/Users/Adward/OneDrive/YelpData/'
@@ -28,12 +29,12 @@ review_class = [260492, 190048, 282115, 591618, 900940]  # 2.6:1.9:2.8:5.9:9.0
 earliest = {'day': 20041018, 'month': 200410, 'year': 2004}
 latest = {'day': 20151224, 'month': 201512, 'year': 2015}
 valid_states = ['AZ', 'NV', 'ON', 'WI', 'QC', 'SC', 'EDH', 'PA', 'MLN', 'BW', 'NC', "IL"]
+applied_categories = {'Debt Relief Services', 'Armenian', 'Spine Surgeons', 'House Sitters', 'Taxidermy', 'Iberian', 'Pita', 'Beer Hall', 'Childproofing', 'Assisted Living Facilities', 'Rhinelandian', 'Oriental', 'Palatine', 'Carpenters', 'Choirs', 'Wok', 'Nursing Schools', 'Surf Shop', 'Perfume', 'Kitchen Incubators', 'Flowers', 'Swiss Food', 'Castles', 'Parenting Classes', 'Ferries', 'Donairs', 'Rest Stops', 'Gerontologists', 'Bike Sharing', 'Piano Stores', 'Trinidadian', 'Translation Services', 'Eastern European', 'College Counseling', 'Community Gardens', 'Wine Tasting Classes', 'Art Restoration', 'Slovakian', 'Backshop', 'Supper Clubs', 'Editorial Services', 'Dialysis Clinics', 'Childbirth Education', 'IP & Internet Law', 'Tax Law', 'Farming Equipment', 'Art Tours', 'Concept Shops', 'Mosques', 'Australian'}
 
 
 # Loading samples from the database & pre-scale
 def load_samples(attr_list, prescale=False, oversampling=(0, 0), elite_expand=False, state_all=False):
     '''
-
     :param attr_list: List[Str], containing the list of features to be selected and encoded
     :param prescale: Bool, (when True) pre-scale features with too large range of values to expedite converging
     :param oversampling: Tuple(Int), double review samples with star classes in range
@@ -44,8 +45,27 @@ def load_samples(attr_list, prescale=False, oversampling=(0, 0), elite_expand=Fa
     '''
     t = time()
     with sqlite3.connect(DB_PATH) as conn:
-        cur = conn.execute('SELECT ' + ",".join(attr_list) + ' FROM ((review JOIN business USING (business_id)) '
-                                                             'JOIN (user NATURAL JOIN user_reside) USING (user_id))')
+        # conn.execute('CREATE TEMP TABLE tmp_b1 (business_id TEXT, avg_star_elite REAL)')
+        # conn.execute('CREATE TEMP TABLE tmp_b2 (business_id TEXT, avg_star_nonelite REAL)')
+        # conn.execute('INSERT INTO tmp_b1 (business_id, avg_star_elite) '
+        #              'SELECT business_id, AVG(average_stars) AS avg_star_elite FROM '
+        #              '(review JOIN user USING (user_id)) WHERE elite!="" GROUP BY business_id')
+        # conn.execute('INSERT INTO tmp_b2 (business_id, avg_star_nonelite) '
+        #              'SELECT business_id, AVG(average_stars) AS avg_star_nonelite FROM '
+        #              '(review JOIN user USING (user_id)) WHERE elite="" GROUP BY business_id')
+
+        # conn.execute('DROP TABLE IF EXISTS bstat_by_elite')
+        # conn.execute('CREATE TABLE bstat_by_elite (business_id TEXT, avg_star_elite REAL, avg_star_nonelite REAL)')
+        # conn.execute('INSERT INTO tmp_b SELECT * FROM '
+        #              '((business LEFT OUTER JOIN tmp_b1 USING (business_id)) '
+        #             'LEFT OUTER JOIN tmp_b2 USING (business_id))')
+
+        # conn.row_factory = sqlite3.Row
+        cur = conn.execute('SELECT ' + ','.join(attr_list) +
+                           ' FROM ('
+                           '(review JOIN (business JOIN b_category_pca USING (business_id)) USING (business_id)) '
+                           'JOIN user '
+                           'USING (user_id) )')
         sample_matrix = []  # feature matrix to return
         targets = []  # class vector
         row_num = 0
@@ -57,11 +77,6 @@ def load_samples(attr_list, prescale=False, oversampling=(0, 0), elite_expand=Fa
             for j in range(1, len(attr_list)):
                 sample[attr_list[j]] = row[j]
 
-            # pre-scale features with too wide range of values
-            if prescale:
-                sample['yelping_since'] /= earliest['year']
-                sample['review_date'] /= earliest['month']
-
             # encode features for business.state
             if ('business.state' in attr_list) and (not state_all) and (sample['business.state'] not in valid_states):
                 sample['business.state'] = 'OTH'  # other 17 states with few business recorded
@@ -69,24 +84,44 @@ def load_samples(attr_list, prescale=False, oversampling=(0, 0), elite_expand=Fa
                 sample['user_state'] = 'OTH'
 
             # Create elite-related features || encode elite-year-number
-            if elite_expand:
-                for year in range(earliest['year']+1, latest['year']+1):
-                    sample['elite'+str(year)] = 0
-                if len(sample['elite']):
-                    elite_years = [int(y) for y in sample['elite'].split('&')]
-                    sample['elite'] = len(elite_years)
-                    for year in elite_years:
-                        sample['elite'+str(year)] = 1
-                else:
-                    sample['elite'] = 0
-            else:
-                if len(sample['elite']):
-                    sample['elite'] = len(sample['elite'].split('&'))
-                else:
-                    sample['elite'] = 0
+            # if elite_expand:
+            #     for year in range(earliest['year']+1, latest['year']+1):
+            #         sample['elite'+str(year)] = 0
+            #     if len(sample['elite']):
+            #         elite_years = [int(y) for y in sample['elite'].split('&')]
+            #         sample['elite'] = len(elite_years)
+            #         for year in elite_years:
+            #             sample['elite'+str(year)] = 1
+            #     else:
+            #         sample['elite'] = 0
+            # else:
+            #     if len(sample['elite']):
+            #         sample['elite'] = len(sample['elite'].split('&'))
+            #     else:
+            #         sample['elite'] = 0
+
+            # encode features of friends_stat
+            # encode features of business_avg_stars_by_elite
+            nan_list = ['avg_review_count', 'avg_votes', 'avg_star_elite', 'avg_star_nonelite']
+            for feat in nan_list:
+                if feat in attr_list and not sample[feat]:
+                    sample[feat] = 0
 
             # encode business.categories features
-            # sample['categories'] =
+            if 'cas' in attr_list:
+                cas = sample['cas'].split(';')
+                del sample['cas']
+                for i in range(3):
+                    sample['ca_'+str(i)] = float(cas[i])
+            #     for ca in applied_categories:
+            #         sample['ca_'+ca] = 0
+            #     if len(sample['categories']):
+            #         categories = sample['categories'].split('&')
+            #         for j in range(len(categories)):
+            #             if categories[j] in applied_categories:
+            #                 sample['ca_' + categories[j]] = 1
+            #     del sample['categories']
+
             # process control & display
             row_num += 1
             # print(sample)
@@ -98,8 +133,8 @@ def load_samples(attr_list, prescale=False, oversampling=(0, 0), elite_expand=Fa
             if oversampling[0] <= targets[-1] <= oversampling[1]:
                 sample_matrix.append(sample)
                 targets.append(targets[-1])
-            # if row_num == 10000:
-            #    break
+                # if row_num == 10000:
+                #    break
 
     print('Done with joining & collecting data from database, using ', time()-t, 's')
     return sample_matrix, targets
@@ -107,7 +142,8 @@ def load_samples(attr_list, prescale=False, oversampling=(0, 0), elite_expand=Fa
 
 def reform_features(sample_matrix, scaling=False):
     t = time()
-    print('Starting reforming categorical features using OneHotDecoding...')
+    print('Start reforming categorical features using OneHotDecoding...')
+    print(sample_matrix[0])
     dictVectorizer = DictVectorizer()
     X = dictVectorizer.fit_transform(sample_matrix).toarray()
     n_features = len(X[0])
@@ -116,8 +152,8 @@ def reform_features(sample_matrix, scaling=False):
         # X = scaler.fit_transform(X)
         X = scale(X)
     print('Feature Num.:', n_features)
-    # target = np.array(targets)
     print('Done with reforming categorical features, using ', time()-t, 's')
+    # print(X[0])
     return X, n_features
 
 
@@ -149,7 +185,9 @@ def train_and_predict(X, y, div, model, n_features):
         scores['mae'].append(mean_absolute_error(y_true=y_test, y_pred=y_pred))
         scores['rmse'].append(mean_squared_error(y_true=y_test, y_pred=y_pred) ** 0.5)
         print(classification_report(y_true=y_test, y_pred=y_pred), '\n')
-        print(confusion_matrix(y_true=y_test, y_pred=y_pred), '\n', time()-t, 's used >>\n')
+        # print(confusion_matrix(y_true=y_test, y_pred=y_pred), '\n', time()-t, 's used >>\n')
+        print(time()-t, 's used >>\n')
+
     # scores = np.array(scores)
     # print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
     print('Done with 5-fold training & cross validating, using ', time()-t, 's')
@@ -158,38 +196,51 @@ def train_and_predict(X, y, div, model, n_features):
     print('F1-Score Weighted: %.3f' % (np.array(scores['f1_weighted']).mean()))
     print('MAE: %.3f' % (np.array(scores['mae']).mean()))
     print('RMSE: %.3f' % (np.array(scores['rmse']).mean()))
-    feature_weights /= 5
+    feature_weights /= len(div)
     # print(feature_weights)
     for i in range(n_features):
-        print('%.1f' % feature_weights[i]*100),
+        print('%.1f' % (feature_weights[i] * 100), end=' '),
 
 if __name__ == '__main__':
+    test_flag = 0
+    for arg in sys.argv:
+        if arg.split('=')[0] == 'test':
+            test_flag = arg.split('=')[1]
     attr_list = [
         'review.stars',  # target value, must be placed at this first place
         'average_stars',
+        # 'avg_friends_star',
+        # 'avg_review_count',
+        # 'avg_star_elite',
+        # 'avg_star_nonelite',
+        # 'avg_votes',
         # 'business.city', # occupies 380 features
-        'business.stars',
-        'business.state',  # occupies 29 -> 13 features
         'business.review_count',
+        'business.stars',
+        # 'business.state',  # occupies 29 -> 13 features
         # 'categories',  # occupies 890 features
+        'cas',
         'checkins',
         'compliments',
-        'elite',  # occupies 12 -> 1 feature(s)
+        # 'elite',  # occupies 12 -> 1 feature(s)
         'fans',
         'review.votes',
         'review_date',
         'user.review_count',
         'user.votes',
         # 'user_state',
-        'weekends_open',  # binary
+        # 'weekends_open',  # binary
         'yelping_since',
         ]
-    samples, targets = load_samples(attr_list, prescale=False, oversampling=(1, 4), state_all=True)
+    samples, targets = load_samples(attr_list, prescale=False, oversampling=(1, 4))
     samples, n_features = reform_features(samples, scaling=False)
     n_samples = len(samples)  # may be different from original n_sample in db !
     print('n_samples:', n_samples)
     # div = StratifiedKFold(targets, n_folds=5)  # 5-Fold Cross Validation
     div = ShuffleSplit(n_samples, n_iter=5, test_size=0.2, random_state=0)
+    if test_flag:
+        div = ShuffleSplit(n_samples, n_iter=1, test_size=0.2, random_state=0)
+
     model = RandomForestClassifier(n_estimators=5, max_features='auto')  # int(math.sqrt(n_features)))
     # model = GradientBoostingClassifier(n_estimators=5, learning_rate=1, max_depth=2, random_state=0)
     train_and_predict(samples, targets, div, model, n_features)
